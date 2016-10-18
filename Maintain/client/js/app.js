1c823fd80dc77bf81956956822fdcbdefcf3b1ba
bijay kanshi <bijay.iit12@gmail.com>
2016-10-19 01:57:50 +0530

    send

--- Maintain/client/js/app.js
+++ Maintain/client/js/app.js
@@ -2,7 +2,7 @@
 var $urlRouterProviderRef = null;
 var $stateProviderRef = null;
 var app = angular.module('app', ["ui.router", "ui.router", "ui.bootstrap",
-        "ui.bootstrap.tpls", "summernote"]);
+        "ui.bootstrap.tpls", "summernote", "colorpicker.module"]);
 app.config(function($stateProvider, $urlRouterProvider) {
    $urlRouterProviderRef = $urlRouterProvider;
    $stateProviderRef = $stateProvider;
--- Maintain/config.js
+++ Maintain/config.js
@@ -1,11 +1,12 @@
 
 module.exports = function(ref) {
 	var app = ref.app;
+	var bodyParser = ref.bodyParser;
 	app.set('view engine', 'html');
 	app.engine('html', require('ejs').renderFile);
 	app.set('views', __dirname + '/client');
 	app.use(ref.express.static(__dirname + '/client'));
-	app.use(ref.bodyParser.json()); // support json encoded bodies
-	app.use(ref.bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json({limit: '50mb'}));
+	app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 };
